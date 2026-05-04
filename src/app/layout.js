03d486e2c41659811f5f82cea8d2456bf601e183
toha245 <mdabdulawal2001@gmail.com>
2026-05-04 18:57:45 +0600

    removed some codes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import MyNavbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "react-hot-toast";
