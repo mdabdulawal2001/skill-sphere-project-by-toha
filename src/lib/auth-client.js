fc46528fc0f199d927809d6b55c169b66ea77078
toha245 <mdabdulawal2001@gmail.com>
2026-05-08 22:48:32 +0600

    added changed url

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://skill-sphere-project-by-toha-p12yp54mg.vercel.app/"
+    baseURL: "https://skill-sphere-project-by-toha.vercel.app/"
     // baseURL: "http://localhost:3000"
 })
 
